f54e9176be70fb25fe638016412e22bea8171f2c
J458795 <160452173+J458795@users.noreply.github.com>
2024-02-20 14:52:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def main():
    = request.fore.get("name")
   return(render_template("main.html",r=name))
 
+@app.route("/imageGPT",methods=["GET","POST"])
+def main():
+  return(render_template("imageGPT.html",r=name))
+
 if __name__ == "__main__":
   app.run()
