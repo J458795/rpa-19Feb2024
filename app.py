88efccdc00df7d25d86c6630103e1782b805a1c6
J458795 <160452173+J458795@users.noreply.github.com>
2024-02-20 15:01:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def main():
    = request.fore.get("r")
   return(render_template("main.html",r=r))
 
-@app.route("/imageGPT",methods=["GET","POST"])
+@app.route("/image_gpt",methods=["GET","POST"])
 def main():
   return(render_template("imageGPT.html"))
 
