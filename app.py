f2d6437df758ccdcef9d6a4a66e3cf588290b38e
J458795 <160452173+J458795@users.noreply.github.com>
2024-02-22 10:24:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def text_result():
 )
         time.sleep(5)
     r = replicate.run(
-        "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+        "stability-ai/stable-diffusion:r8_ce57ooEmWgSqmS6BqjCBGSWW2waTZTg3N2bhg",
         input={
             "prompt": q,
         }
